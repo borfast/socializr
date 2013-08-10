a4bba5c66649e83eef67c9ad0ab10a439d29bb1a
borfast <borfast@gmail.com>
2013-08-11 00:35:45 +0100

    Back to the original Twitter library...

--- src/Borfast/Socializr/TwitterEngine.php
+++ src/Borfast/Socializr/TwitterEngine.php
@@ -12,23 +12,26 @@ class TwitterEngine implements SocializrInterface
     public function __construct($config ,$auth)
     {
         $twitter_config = array(
-            'access_token' => array(
-                'token'  => $auth['oauth_access_token'],
-                'secret' => $auth['oauth_access_token_secret'],
-            ),
-            'oauth_options' => array(
-                'consumerKey' => $config['consumer_key'],
-                'consumerSecret' => $config['consumer_secret'],
-            )
+            'oauth_access_token' => $auth['oauth_access_token'],
+            'oauth_access_token_secret' => $auth['oauth_access_token_secret'],
+            'consumer_key' => $config['consumer_key'],
+            'consumer_secret' => $config['consumer_secret'],
         );
 
-        $this->twitter = new Twitter($twitter_config);
+        $this->twitter = new \TwitterAPIExchange($twitter_config);
     }
 
 
     public function post($content)
     {
-        $response = $this->twitter->statuses->update($content);
+        $url = 'https://api.twitter.com/1.1/statuses/update.json';
+        $requestMethod = 'POST';
+        $postfields = array(
+            'status' => $content,
+        );
+        $response = $this->twitter->buildOauth($url, $requestMethod)
+            ->setPostfields($postfields)
+            ->performRequest();
 
         return $response;
     }
