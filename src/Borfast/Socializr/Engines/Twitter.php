51f71e04f27fd469c118949d5617860a803b4257
borfast <borfast@gmail.com>
2013-08-13 23:23:31 +0100

    Trying to fix Twitter...

--- src/Borfast/Socializr/Engines/Twitter.php
+++ src/Borfast/Socializr/Engines/Twitter.php
@@ -2,36 +2,22 @@
 
 namespace Borfast\Socializr\Engines;
 
-use Borfast\Socializr\SocializrInterface;
+use Borfast\Socializr\Engines\AbstractEngine;
+use OAuth\Common\Storage\TokenStorageInterface;
 
-class Twitter implements SocializrInterface
+class Twitter extends AbstractEngine
 {
-    protected $twitter = null;
-
-
-    public function __construct($config)
-    {
-        $twitter_config = array(
-            // 'oauth_access_token' => $auth['oauth_access_token'],
-            // 'oauth_access_token_secret' => $auth['oauth_access_token_secret'],
-            'consumer_key' => $config['consumer_key'],
-            'consumer_secret' => $config['consumer_secret'],
-        );
-
-        $this->twitter = new \TwitterAPIExchange($twitter_config);
-    }
-
+    public static $PROVIDER = 'Twitter';
 
     public function post($content)
     {
-        $url = 'https://api.twitter.com/1.1/statuses/update.json';
+        $path = '/statuses/update.json';
         $method = 'POST';
-        $post_fields = array(
+        $params = array(
             'status' => $content,
         );
-        $response = $this->twitter->buildOauth($url, $method)
-            ->setPostfields($post_fields)
-            ->performRequest();
+
+        $response = $this->service->request($path, 'POST', $params);
 
         return $response;
     }
@@ -39,29 +25,30 @@ class Twitter implements SocializrInterface
 
     public function authorize()
     {
-        // Instantiate the twitter service using the credentials, http client and storage mechanism for the token
-        /** @var $twitterService Twitter */
-        $twitterService = $serviceFactory->createService('twitter', $credentials, $storage);
+        $token = $this->service->requestRequestToken();
+        $url = $this->service->getAuthorizationUri(array('oauth_token' => $token->getRequestToken()));
+        header('Location: ' . $url);
+        exit;
+    }
 
-        if(!empty($_GET['oauth_token'])) {
-            $token = $storage->retrieveAccessToken('Twitter');
-            // This was a callback request from twitter, get the token
-            $twitterService->requestAccessToken($_GET['oauth_token'], $_GET['oauth_verifier'], $token->getRequestTokenSecret());
 
-            // Send a request now that we have access token
-            $result = json_decode($twitterService->request('account/verify_credentials.json'));
+    public function storeOauthToken($params)
+    {
+        $token = $this->storage->retrieveAccessToken('Twitter');
+        $this->service->requestAccessToken($params['oauth_token'], $params['oauth_verifier'], $token->getRequestTokenSecret());
+    }
 
-            echo 'result: <pre>' . print_r($result, true) . '</pre>';
 
-        } elseif(!empty($_GET['go']) && $_GET['go'] == 'go') {
-            // extra request needed for oauth1 to request a request token :-)
-            $token = $twitterService->requestRequestToken();
+    public function getUid()
+    {
+        $profile = $this->getProfile();
 
-            $url = $twitterService->getAuthorizationUri(array('oauth_token' => $token->getRequestToken()));
-            header('Location: ' . $url);
-        } else {
-            $url = $currentUri->getRelativeUri() . '?go=go';
-            echo "<a href='$url'>Login with Twitter!</a>";
-        }
+        return $profile['id'];
+    }
+
+    public function getProfile()
+    {
+        $response = $this->service->request('/me');
+        return json_decode($response, true);
     }
 }
