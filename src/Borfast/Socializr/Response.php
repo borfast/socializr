35c4aa0660ec2b2f3035f5f46843ed13da0b0a82
borfast <borfast@gmail.com>
2014-01-31 17:01:58 +0000

    Add post URL to the Response object.

--- src/Borfast/Socializr/Response.php
+++ src/Borfast/Socializr/Response.php
@@ -7,6 +7,7 @@ class Response
     public $raw_response;
     public $provider;
     public $post_id;
+    public $post_url;
 
 
     public function setRawResponse($raw_response)
@@ -24,6 +25,12 @@ class Response
     public function setPostId($post_id)
     {
         $this->post_id = $post_id;
+        return $this;
+    }
+
+    public function setPostUrl($post_url)
+    {
+        $this->post_url = $post_url;
         return $this;
     }
 
