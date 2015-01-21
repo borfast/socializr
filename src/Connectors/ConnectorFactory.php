d300c7fafec597ede915756c216a4292f154a867
Raúl Santos <borfast@gmail.com>
2015-01-21 01:25:41 +0000

    Reinstate hack for Facebook API 2.x

--- src/Connectors/ConnectorFactory.php
+++ src/Connectors/ConnectorFactory.php
@@ -104,14 +104,21 @@ class ConnectorFactory
             $api_version = '2.2';
         }
 
+        // Temporary (or so I hope) hack to overcome PHPoAuthLib not being ready
+        // for Facebook's Graph API 1.0 deprecation.
+        $uri = null;
+        if ($provider == 'Facebook') {
+            $uri = new Uri('https://graph.facebook.com/v2.1/');
+        }
+
         // Finally, create the service already!
         $service = $service_factory->createService(
             $config['service'],
             $credentials,
             $storage,
-            $config['scopes']
-            /*null,
-            $api_version*/
+            $config['scopes'],
+            $uri
+            //$api_version
         );
 
 
