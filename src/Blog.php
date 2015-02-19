4b7608ced356a3ccf8e1816109ffff198b5aee14
Raúl Santos <borfast@gmail.com>
2015-02-19 00:02:01 +0000

    Add more fields for Tumblr blogs

--- src/Blog.php
+++ src/Blog.php
@@ -7,6 +7,9 @@ class Blog
     public $title = '';
     public $posts = 0;
     public $name = '';
+    public $updated;
     public $description = '';
+    public $ask = false;
+    public $ask_anon;
     public $likes = 0;
 }
